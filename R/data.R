d83a3383b58eaad5df8d7b5a398b2f12779a75d0
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2023-08-14 12:57:00 +0800

    Document

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' Brunei map
+#'
+#' Brunei map
+#'
+#' Brunei map
+#'
+#' @name bruneimap
+#' @rdname bruneimap
+#' @format An `sf` object
+"dis_sf"
+
+#' @rdname bruneimap
+#' @format NULL
+"mkm_sf"
+
+#' @rdname bruneimap
+#' @format NULL
+"kpg_sf"
+
+#' Brunei kampong data
+#'
+#' Brunei kampong data
+#'
+#' Brunei kampong data
+#'
+#' @format A tibble containing kampong, mukim, district information. All rows
+#'   correspond to an area on the map. Useful to combine with other unit-level
+#'   information for plotting.
+"bnkpg_df"
