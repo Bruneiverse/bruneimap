b25851be46bc9227234352d8b28a2c718f83c9e0
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-03-01 11:29:44 +0800

    Add census data

--- R/data.R
+++ R/data.R
@@ -27,3 +27,23 @@
 #'   correspond to an area on the map. Useful to combine with other unit-level
 #'   information for plotting.
 "bnkpg_df"
+
+#' Brunei census data 2021
+#'
+#' Brunei census data 2021
+#'
+#' Brunei census data 2021
+#'
+#' @format A tibble containing the population, split into male/female or
+#'   Bruneian/PR/foreigners for each kampong. Also contains number of households
+#'   and occupied living quarters.
+#'
+#' @references DEPS. “The Population and Housing Census Report (BPP) 2021:
+#'   Demographic, Household and Housing Characteristics.” Department of Economic
+#'   Planning and Statistics, Ministry of Finance and Economy, Brunei
+#'   Darussalam, October 2022. URL:
+#'   \url{https://deps.mofe.gov.bn/SitePages/Population.aspx}
+#'
+"bn_census2021"
+
+
