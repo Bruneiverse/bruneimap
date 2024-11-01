f65aad91c8892ee2b1e3ed66da8846c68ff29bee
Angela-29 <24b2033@ubd.edu.bn>
2024-11-01 12:10:27 +0800

    R file for Question 1-3

---
+++ Question1.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(bruneimap)
+library(ggrepel)
+library(kernlab)
+library(osrm)
+library (osmdata)
+library(readxl)
+packageVersion ("osmdata")
+
+#Getting data from OSM
+h <- 
+  opq("brunei") %>%
+  add_osm_feature(
+    key = "amenity",
+    value = "hospital"
+  ) %>%
+  osmdata_sf()
+glimpse(h$osm_polygons)
+
+hospitalsh_sf <-
+  h$osm_polygons |>
+  as_tibble() |>  
+  st_as_sf() |> 
+  select(osm_id, name, amenity) |>
+  st_centroid()  
+hospitalsh_sf <- hospitalsh_sf %>%
+  filter(!is.na(amenity))
+
+glimpse(hospitalsh_sf)
+view(hospitalsh_sf)
+
+#Read Excel file; data from excel file 
+hospitalexcel <- read_excel("excel coordinates.xlsx")
+
+#Check for NAs
+na_latitude <- sum(is.na(hospitalexcel$Latitude))
+na_longitude <- sum(is.na(hospitalexcel$Longitude))
+
+if (na_latitude > 0 | na_longitude > 0) {
+  print(paste("NAs found in Latitude:", na_latitude, "Longitude:", na_longitude))
+}
+
+#Convert to numeric if necessary
+hospitalexcel$Latitude <- as.numeric(as.character(hospitalexcel$Latitude))
+hospitalexcel$Longitude <- as.numeric(as.character(hospitalexcel$Longitude))
+
+#Remove rows with NAs in Latitude and Longitude
+hospitalexcel <- hospitalexcel %>% drop_na(Latitude, Longitude)
+view(hospitalexcel)
+# Convert to sf object
+hpex <- st_as_sf(hospitalexcel, coords = c("Longitude", "Latitude"), crs = 4326, remove = TRUE) 
+view(hpex)
+
+
+#Plotiing
+plotm <- ggplot() +
+  geom_sf(data = mkm_sf, aes(fill = district), alpha = 0.3) +
+  geom_sf(data = st_filter(hospitalsh_sf, mkm_sf), size = 2, color = "black") +  # Existing hospitals
+  geom_sf(data = st_filter(hpex, mkm_sf), size = 2, color = "black") + #Hosptials from Excel
+  labs(title = "Hospitals around Brunei",
+       fill = "District")
+plotm
