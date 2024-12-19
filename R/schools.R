3099419ed92b8e016b097549da18b42f111cdc29
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-12-19 20:43:19 +0800

    Update docs

--- R/schools.R
+++ R/schools.R
@@ -13,7 +13,7 @@
 #' - [Education Statistics and Indicators
 #'   Handbook](https://www.moe.gov.bn/DocumentDownloads/Forms/AllItems.aspx?RootFolder=%2fDocumentDownloads%2fEducation%20Statistics%20and%20Indicators%20Handbook&FolderCTID=0x012000686799B237388E4B93D164D060CACEC6).
 #'   Ministry of Education, Brunei.
-#' - [Summarised version, Y2023](https://deps.mofe.gov.bn/SitePages/Statistical%20Publications.aspx|)
+#' - [Summarised version, Y2023](https://deps.mofe.gov.bn/SitePages/Statistical%20Publications.aspx)
 #' @examples
 #' library(ggplot2)
 #' ggplot() +
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 <!-- badges: start -->
 
-[![R-CMD-check](https://github.com/propertypricebn/bruneimap/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/propertypricebn/bruneimap/actions/workflows/R-CMD-check.yaml)
+[![R-CMD-check](https://github.com/Bruneiverse/bruneimap/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/Bruneiverse/bruneimap/actions/workflows/R-CMD-check.yaml)
 <!-- badges: end -->
 
 Provides Brunei GIS data in the form of a `sf` (simple features) object
