5fa86133ba4bc388c67d94c38c281584ee2a801c
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-12-21 11:43:22 +0800

    Update masjid data set

--- R/masjid.R
+++ R/masjid.R
@@ -2,7 +2,10 @@
 #'
 #' @format A tibble containing the name and lat/long coordinates of masjids in
 #'   Brunei
-#' @references https://www.mora.gov.bn/senaraimasjid/forms/viewall.aspx
+#'
+#' @source
+#' - Ministry of Religious Affairs Annual Statistics 2021. URL: <https://www.mora.gov.bn/SiteCollectionDocuments/Statistik/Perangkaan%20Tahunan%20KHEU%202021.pdf>
+#' - Senarai Masjid di Negara Brunei Darussalam. URL: <https://www.mora.gov.bn/senaraimasjid/forms/viewall.aspx
 #' @examples
 #' dplyr::glimpse(masjid)
 "masjid"
--- data-raw/masjid/masjid.R
+++ data-raw/masjid/masjid.R
@@ -1,3 +1,15 @@
 library(tidyverse)
-masjid <- read_csv(here::here("data-raw/masjid/masjid.csv"))
+
+# Notes:
+# - masjid_old.csv from Algebros (least amount of information)
+# - masjid.csv from The R Ones
+# - bruneimosque.csv from HWR (most complete)
+#
+# Edit bruneimosque.csv for future updates!
+
+masjid <-
+  read_csv(here::here("data-raw/masjid/bruneimosque.csv")) |>
+  mutate(
+    district = gsub("Brunei Muara", "Brunei-Muara", district)
+  )
 usethis::use_data(masjid, overwrite = TRUE, compress = "xz")
